e4c0e2b282cb74f26115dfc94511df84d1483064
HanSummer <xiahl0916@163.com>
2026-03-21 00:42:08 +0800

    fix:移动端样式优化

--- themes/heo/components/PostCopyright.js
+++ themes/heo/components/PostCopyright.js
@@ -27,21 +27,21 @@ export default function PostCopyright() {
     <section className='dark:text-gray-300 mt-6 mx-1 '>
       <ul className='overflow-x-auto whitespace-normal text-sm dark:bg-gray-900 bg-gray-100 p-5 leading-8 border-l-2 border-indigo-500'>
         <li>
-          <strong className='mr-2'>{locale.COMMON.AUTHOR}:</strong>
-          <SmartLink href={'/about'} className='hover:underline'>
+          <strong className='mr-2 inline-block'>{locale.COMMON.AUTHOR}:</strong>
+          <SmartLink href={'/about'} className='whitespace-normal break-all hover:underline'>
             {siteConfig('AUTHOR')}
           </SmartLink>
         </li>
         <li>
-          <strong className='mr-2'>{locale.COMMON.URL}:</strong>
+          <strong className='mr-2 inline-block'>{locale.COMMON.URL}:</strong>
           <a
-            className='whitespace-normal break-words hover:underline'
+            className='whitespace-normal break-all hover:underline'
             href={path}>
             {path}
           </a>
         </li>
         <li>
-          <strong className='mr-2'>{locale.COMMON.COPYRIGHT}:</strong>
+          <strong className='mr-2 inline-block'>{locale.COMMON.COPYRIGHT}:</strong>
           {locale.COMMON.COPYRIGHT_NOTICE}
         </li>
         {siteConfig('HEO_ARTICLE_NOT_BY_AI', false, CONFIG) && (
